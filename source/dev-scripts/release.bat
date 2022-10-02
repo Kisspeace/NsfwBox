2fa365044f067fefb2013d95676823b12aeb612f
Kisspeace <Kisspeace@github.com>
2022-10-02 11:19:09 +0300

    Android64

--- source/dev-scripts/release.bat
+++ source/dev-scripts/release.bat
@@ -2,9 +2,10 @@
 
 echo Release tag: %~1 
 
+SET dir_backup=%cd%
 SET tag=%~1
 SET script_dir=%~dp0
-SET project_dir=%script_dir%..\..\
+SET project_dir=%script_dir%..\..
 SET work_dir=%project_dir%\release
 SET archivedir=%work_dir%\NsfwBox
 SET tool="C:\Program Files\7-Zip\7zG.exe"
@@ -12,9 +13,11 @@ SET tool="C:\Program Files\7-Zip\7zG.exe"
 MKDIR %work_dir%
 cd %work_dir%
 
+COPY %project_dir%\Android64\Release\NsfwBox\bin\NsfwBox.apk %work_dir%\Kisspeace.NsfwBox.%tag%.apk
 CALL :pack Win32 ins32
 CALL :pack Win64 ins64
 
+cd %dir_backup%
 EXIT /B 0
 
 :pack
