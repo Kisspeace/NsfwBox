e23e36892a5f511027e0175c37f855762364d6fc
Kisspeace <Kisspeace@github.com>
2022-10-21 15:34:25 +0300

    Dev script that copy all important files(assets, images, libs..) to release directory

---
+++ source/dev-scripts/prepare-to-work.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET script_dir=%~dp0
+SET project_dir=%script_dir%..\..
+
+ECHO WIndows 32
+MKDIR %project_dir%\Win32\Release
+ROBOCOPY /S %project_dir%\assets\themes %project_dir%\Win32\Release\themes
+COPY %project_dir%\libs\win32\*.dll %project_dir%\Win32\Release\
+
+ECHO WIndows 64
+MKDIR %project_dir%\Win64\Release
+ROBOCOPY /S %project_dir%\assets\themes %project_dir%\Win64\Release\themes
+COPY %project_dir%\libs\win64\*.dll %project_dir%\Win64\Release\
