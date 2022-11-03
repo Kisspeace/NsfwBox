725941f815ff1834e79ec3eadbab61571c163565
Kisspeace <Kisspeace@github.com>
2022-11-03 11:09:42 +0300

    Downloaders based on YDW

--- source/NsfwBox.DownloadManager.pas
+++ source/NsfwBox.DownloadManager.pas
@@ -5,42 +5,38 @@ unit NsfwBox.DownloadManager;
 interface
 uses
   Classes, NetHttpClient.Downloader, Net.HttpClient, Net.HttpClientComponent,
-  System.Threading, system.Generics.Collections, system.SyncObjs,
-  System.SysUtils;
+  System.Threading, system.Generics.Collections, System.SysUtils, YDW.Threading,
+  NsfwBox.Graphics;
 
 type
 
-  TOnCreateDownloader =
-    procedure (Sender: Tobject; const ADownloader: TNetHttpDownloader) of object;
+  TNBoxDownloader = Class(TNetHttpDownloader)
+    public
+      Tab: TNBoxTab;
+  End;
 
+  TOnCreateDownloader = procedure (Sender: Tobject; const ADownloader: TNBoxDownloader) of object;
   TOnStartDownloader = TOnCreateDownloader;
 
   TDownloaderList = TList<TNetHttpDownloader>;
   TDownloaderObjList = TObjectList<TNetHttpDownloader>;
 
-  TNBoxDownloadManager = class(TComponent)
+  TNBoxDownloadManager = class(TGenericYDWQueuedThreadObject<TNBoxDownloader>)
     private
-      FNowDestroy: boolean;
       FSynchronizeEvents: boolean;
-      FLock: TCriticalSection;
-      FMainTask: ITask;
-      FQueue: TDownloaderList;
-      FMaxThreadCount: int64;
       FOnCreateDownloader: TOnCreateDownloader;
       FOnStartDownloader: TOnStartDownloader;
-      procedure SetMaxThreadCount(const value: int64);
-      function GetMaxThreadCount: int64;
       procedure SetSynchronizeEvents(const value: boolean);
       function GetSynchronizeEvents: boolean;
-      procedure StartMainTask;
-      procedure CancelAll;
+    protected
+      procedure SubThreadExecute(AItem: TNBoxDownloader); override;
     public
-      function AddDownload(AUrl: string; AFilename: string): TNetHttpDownloader;
-      property MaxThreadCount: int64 read GetMaxThreadCount write SetMaxThreadCount default 5;
+      function AddDownload(AUrl: string; AFilename: string; ATab: TNBoxTab = nil): TNBoxDownloader;
+      procedure AbortDownload(ADownloader: TNBoxDownloader);
       property SynchronizeEvents: boolean read GetSynchronizeEvents write SetSynchronizeEvents default true;
       property OnCreateDownloader: TOnCreateDownloader read FOnCreateDownloader write FOnCreateDownloader;
       property OnStartDownloader: TOnStartDownloader read FOnStartDownloader write FOnStartDownloader;
-      constructor Create(AOwner: TComponent); override;
+      constructor Create; override;
       destructor Destroy; override;
   end;
 
@@ -48,258 +44,80 @@ implementation
  uses unit1;
 { TNBoxDownloadManager }
 
-function TNBoxDownloadManager.AddDownload(AUrl,
-  AFilename: string): TNetHttpDownloader;
+procedure TNBoxDownloadManager.AbortDownload(ADownloader: TNBoxDownloader);
 begin
-  Result := TNetHttpDownloader.Create(Self);
+  Self.AbortItem(ADownloader);
+end;
+
+function TNBoxDownloadManager.AddDownload(AUrl,
+  AFilename: string; ATab: TNBoxTab): TNBoxDownloader;
+begin
+  Result := TNBoxDownloader.Create;
   With Result do begin
-    Url           := AUrl;
-    Filename      := AFilename;
-    AutoRetry       := true;
+    Url       := AUrl;
+    Filename  := AFilename;
+    AutoRetry := True;
+    Tab       := ATab;
   end;
 
   if Assigned(OnCreateDownloader) then
     OnCreateDownloader(Self, Result);
 
-  FLock.enter;
-  try
-    FQueue.Add(Result);
-    if not Assigned(FMainTask) then
-      StartMainTask;
-
-  finally
-    FLock.Leave;
-  end;
+  Self.QueueAdd(Result);
 end;
 
-procedure TNBoxDownloadManager.CancelAll;
-begin
-  FLock.enter;
-  try
-    if Assigned(FMainTask) then begin
-      FMainTask.Cancel;
-    end;
-  finally
-    FLock.Leave;
-  end;
-end;
-
-constructor TNBoxDownloadManager.Create(AOwner: TComponent);
+constructor TNBoxDownloadManager.Create;
 begin
   inherited;
-  FNowDestroy := false;
   FSynchronizeEvents := true;
-  FLock := TCriticalSection.Create;
-  FQueue := TDownloaderList.Create;
-  Self.FMaxThreadCount := 5;
-  FMainTask := nil;
 end;
 
 destructor TNBoxDownloadManager.Destroy;
-var
-  I: integer;
 begin
-  FLock.enter;
-  try
-    FNowDestroy := true;
-    OnStartDownloader := nil;
-  finally
-    FLock.Leave;
-  end;
-
-  if Assigned(FMainTask) then begin
-    CancelAll;
-    if TThread.Current.ThreadID = MainThreadId then begin
-      try
-        while not TTask.WaitForAll([FMainTask], 100) do
-          CheckSynchronize(10);
-      except
-
-      end;
-    end;
-  end;
-
-  For I := 0 to FQueue.Count - 1 do
-    FQueue[I].Free;
-  FQueue.Clear;
-  
-  FLock.Free;
-  FQueue.Free;
   inherited;
 end;
 
 function TNBoxDownloadManager.GetSynchronizeEvents: boolean;
 begin
-  FLock.Enter;
+  FLock.BeginRead;
   try
     Result := FSynchronizeEvents;
   finally
-    FLock.Leave;
+    FLock.EndRead;
   end;
-end;
-
-function TNBoxDownloadManager.GetMaxThreadCount: int64;
-begin
-  Result := TInterlocked.Read(FMaxThreadCount);
 end;
 
 procedure TNBoxDownloadManager.SetSynchronizeEvents(const value: boolean);
 begin
-  FLock.Enter;
+  FLock.BeginWrite;
   try
     FSynchronizeEvents := Value;
   finally
-    FLock.Leave;
+    FLock.EndWrite;
   end;
 end;
 
-procedure TNBoxDownloadManager.SetMaxThreadCount(const value: int64);
+procedure TNBoxDownloadManager.SubThreadExecute(AItem: TNBoxDownloader);
+const
+  WAIT_TIMEOUT = 10;
 begin
-  TInterLocked.Exchange(FMaxThreadCount, value);
-end;
-
-procedure TNBoxDownloadManager.StartMainTask;
-begin
-  FMainTask := TTask.Create(
-  procedure
-  var
-    I, Count, Pos: integer;
-    id: string;
-    Loaders: TDownloaderObjList;
-    LoadersUpdated: boolean;
-
-    procedure Update;
-    begin
-      try
-      FLock.enter;
-      try
-        LoadersUpdated := ( FQueue.Count > 0 );
-        if LoadersUpdated then begin
-          Loaders.AddRange(FQueue);
-          FQueue.Clear;
-        end;
-      finally
-        FLock.Leave;
-      end;
-      except on E: Exception do
-        SyncLog(E, 'EXCEPTION: On Update: ');
-      end;
-    end;
-
-    function CanStart(A: TNetHttpDownloader): boolean;
-    begin
-      Result := not ( A.IsAborted or A.IsRunning );
-    end;
-
-    function RunningCount: integer;
-    var
-      I: integer;
-    begin
-      Result := 0;
-      for I := 0 to Loaders.Count - 1 do begin
-        if Loaders[I].IsRunning then
-          Inc(Result);
-      end;
-    end;
-
-  begin
+  try
     try
-    try
-      id := TThread.Current.ThreadID.ToString;
-      Loaders := TDownloaderObjList.Create;
+       AItem.Start;
 
-      Update;
-
-      Pos := 0;  // Loaders.Items[0]; - Starts from first item.
-      while LoadersUpdated do begin
-        LoadersUpdated := false;
-
-        for I := Pos to Loaders.Count - 1 do begin
-          var Item: TNetHttpDownloader;
-          Item := Loaders[I];
-          Pos := I;
-
-          FMainTask.CheckCanceled;
-          while ( RunningCount >= MaxThreadCount ) do begin
-            FMainTask.CheckCanceled;
-            Sleep(100);
-          end;
-
-          With Item do begin
-            if CanStart(Item) then begin
-
-              if Assigned(OnStartDownloader) then begin
-                if SynchronizeEvents then
-                  TThread.Synchronize(TThread.Current, procedure begin OnStartDownloader(Self, Item) end)
-                else
-                  OnStartDownloader(Self, Item);
-              end;
-
-              try
-                Start;
-              except
-                On E: EXception do begin
-                  SyncLog(E, 'Exception: Downloader On Start: ');
-                end;
-              end;
-
-            end;
-          end;
-
-        end;
-
-        while ( RunningCount > 0 ) do begin
-          FMainTask.CheckCanceled;
-          sleep(10);
-          Update;
-          if LoadersUpdated then begin
-            Break;
-          end;
-        end;
-
-      end;
+       while AItem.IsRunning do begin
+         Sleep(WAIT_TIMEOUT);
+         if TThread.Current.CheckTerminated then exit;
+       end;
 
     finally
-
-      for I := 0 to Loaders.Count - 1 do begin
-        if Loaders[I].IsRunning then
-          Loaders.items[I].AbortRequest;
-      end;
-
-      while ( RunningCount > 0 ) do
-        sleep(50);
-
-      Loaders.Free;
-      //SyncLog('Exit from TNBoxDownloadManager main thread');
-      FLock.enter;
-      try
-        if not FNowDestroy then begin
-          Count := FQueue.Count;
-
-          if ( Count > 0 ) then
-            StartMainTask
-          else
-            FMainTask := nil;
-        end;
-      finally
-        FLock.Leave;
-      end;
-
+      FreeAndNil(AItem);
     end;
-    except
+  Except
+    On E: Exception do
+      Unit1.SyncLog(E, 'TNBoxDownloadManager.SubThreadExecute');
+  end;
 
-      On E: EOperationCancelled do begin
-        // ignore
-      end;
-
-      On E: exception do begin
-        SyncLog(E, 'TNBoxDownloadManager: ');
-      end;
-
-    end;
-  end);
-  FmainTask.Start;
 end;
-
 
 end.
